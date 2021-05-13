f0a92f66a491668b8a1876f066b06ed1b906e349
Winter Dresden <winterdresden@Winters-MacBook-Air.local>
2021-05-13 13:36:45 -0600

    landingpage

--- client/src/components/LandingPage.js
+++ client/src/components/LandingPage.js
@@ -2,10 +2,10 @@ import React from "react";
 import { Link } from "react-router-dom";
 // import blackflower from './icons/black-flower.png';
 import blackflower from './icons/ok-flower.png';
-import blackglobe from './icons/black-globe.png';
+import blackglobe from './icons/ok.globe.png';
 import spiral from './icons/spiral.png';
 import whiteFlower from './icons/whiteFlower.png';
-import whiteGlobe from './icons/whiteGlobe.png';
+import whiteGlobe from './icons/ok.globe.white.png';
 import whiteSpiral from './icons/whiteSpiral.png';
 import Flower from "./Flower";
 
@@ -20,13 +20,13 @@ export default function LandingPage(props) {
             <Link to={"/PastFlowers"}><img src={spiral} alt="spiral, nav to past flowers" id="landing"/></Link>
             
             <Link to={"/Create"}><img src={blackflower} alt="flower, nav to create" id="landing"/></Link>
-            <Link to={"/Global"}><img src={blackglobe} alt="globe, nav to global" id="landing"/></Link> 
+            <Link to={"/Global"}><img src={blackglobe} alt="globe, nav to global" id="landingGlobe"/></Link> 
             </div>
         :  
             <div className="DarkLanding">
             <Link to={"/PastFlowers"}><img src={whiteSpiral} alt="spiral, nav to past flowers" id="landing"/></Link>
             <Link to={"/Create"}><img src={whiteFlower} alt="flower, nav to create" id="landing"/></Link>
-            <Link to={"/Global"}><img src={whiteGlobe} alt="globe, nav to global" id="landing"/></Link>
+            <Link to={"/Global"}><img src={whiteGlobe} alt="globe, nav to global" id="landingGlobe"/></Link>
             </div>
        } 
         <div className="landingText">
