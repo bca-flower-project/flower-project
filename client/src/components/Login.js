231575413c837b328adcf521c59633513f4b4f4b
Gabe Koss <gabe.koss@gmail.com>
2021-06-17 12:27:21 -0400

    Login styles

--- client/src/components/Login.js
+++ client/src/components/Login.js
@@ -1,5 +1,6 @@
 import React, { useContext } from "react";
-import { Container, Button, Card } from "react-bootstrap";
+import "./Login.scss";
+import { Container, Button, Card, Row, Col } from "react-bootstrap";
 import { AuthContext } from "../contexts/AuthContext";
 
 export default function Login() {
@@ -7,22 +8,26 @@ export default function Login() {
 
   return (
     <Container className="loginWrapper">
-      <h1>Please sign in!</h1>
-      <Card>
-        <Card.Body>
-          <h2 className="text-center mb-4" style={{ color: "black" }}>
-            Log In
-          </h2>
-          <Button
-            onClick={login}
-            className="w-100"
-            style={{ background: "black" }}
-            type="submit"
-          >
-            Google Login
-          </Button>
-        </Card.Body>
-      </Card>
+      <Row className="col-6 offset-3 justify-content-center">
+        <Col>
+          <h1>Please sign in!</h1>
+          <Card>
+            <Card.Body>
+              <h2 className="text-center mb-4" style={{ color: "black" }}>
+                Log In
+              </h2>
+              <Button
+                onClick={login}
+                className="w-100"
+                style={{ background: "black" }}
+                type="submit"
+              >
+                Google Login
+              </Button>
+            </Card.Body>
+          </Card>
+        </Col>
+      </Row>
     </Container>
   );
 }
