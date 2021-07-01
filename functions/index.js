f934821ad898195977ef04ed7cdd8614639561c3
Gabe Koss <gabe.koss@gmail.com>
2021-07-01 09:50:08 -0400

    send email with functions

--- functions/index.js
+++ functions/index.js
@@ -1,15 +1,75 @@
 const functions = require("firebase-functions");
 const admin = require("firebase-admin");
-admin.initializeApp()
+const sgMail = require("@sendgrid/mail");
+
+admin.initializeApp();
+
+const SENDGRID_API_KEY = functions.config().sendgrid.key;
+const SENDGRID_SENDER = functions.config().sendgrid.sender;
+const NEW_USER_EMAIL_TEMPLATE_ID = "d-7a1bc4cdc3d84efdab47c7e2edf6909d";
+const NEW_FLOWER_TEMPLATE_ID = "d-dca0af08c28148b5975bdc311c557121";
+const HAPPY_BIRTHDAY_EMAIL_TEMPLATE_ID = "d-7930e6c61d964ab19f63ea031e312701"
+
+sgMail.setApiKey(SENDGRID_API_KEY);
+
+const doSendEmail = async (to, templateId, dynamic_template_data) => {
+  try {
+    const msg = {
+      to,
+      from: SENDGRID_SENDER,
+      templateId,
+      dynamic_template_data,
+    };
+
+    functions.logger.info(
+      "Sending new flower email to " +
+        userData.email +
+        " " +
+        JSON.stringify(msg)
+    );
+
+    const [response] = await sgMail.send(msg);
+
+    functions.logger.info("Email sent.");
+  } catch (error) {
+    functions.logger.error(JSON.stringify(error));
+  }
+};
+
+exports.flowerOnCreate = functions.firestore
+  .document("/user/{userId}/flower/{flowerId}")
+  .onCreate(async (snap, ctx) => {
+    let userData;
+    await admin
+      .firestore()
+      .collection("user")
+      .doc(ctx.params.userId)
+      .get()
+      .then((doc) => (userData = doc.data()));
+    await doSendEmail(userData.email, NEW_FLOWER_TEMPLATE_ID, {
+      subject: "Nice work! You created a flower!",
+      name: userData.name,
+    });
+    return { success: true };
+  });
+
+exports.newUserEmail = functions.firestore
+  .document("user/{userId}")
+  .onCreate((snap, ctx) => {
+    const userData = snap.data();
+    await doSendEmail(userData.email, NEW_USER_EMAIL_TEMPLATE_ID, {
+      subject: "Welcome to flowers.community",
+      name: userData.name,
+    });
+    return { success: true };
+  });
 
 exports.happyBirthday = functions.pubsub
   .schedule("every 24 hours")
   .onRun(async (context) => {
-    const today = new Date()
-    const currMonth = `${today.getMonth() + 1}`.padStart(2, '0')
-    const currDay = `${today.getDate()}`.padStart(2, '0')
-
-    functions.logger.info("Querying users with birthday of " + currMonth + "/" + currDay);
+    const today = new Date();
+    const currMonth = `${today.getMonth() + 1}`.padStart(2, "0");
+    const currDay = `${today.getDate()}`.padStart(2, "0");
 
     const users = await admin
       .firestore()
@@ -17,16 +77,17 @@ exports.happyBirthday = functions.pubsub
       .where("monthOfBirth", "==", currMonth)
       .where("dayOfBirth", "==", currDay)
       .get()
-			.then((querySnapshot) => {
-        querySnapshot.forEach((doc) => {
-					functions.logger.info(
-            JSON.stringify(doc.data()),
-            {structuredData: true}
-          );
+      .then((querySnapshot) => {
+        querySnapshot.forEach(async (doc) => {
+          const userData = doc.data();
+          doSendEmail(userData.email, HAPPY_BIRTHDAY_EMAIL_TEMPLATE_ID, {
+            subject: "Happy Birthday from flowers.community",
+            name: userData.name,
+          });
         });
-    })
-    .catch((error) => {
+      })
+      .catch((error) => {
         functions.logger.error("Error getting documents: ", error);
-    });
+      });
     return null;
   });
