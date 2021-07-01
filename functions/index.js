9c7975852b9b10c8e535e31fa7b9279f015c9e47
Gabe Koss <gabe.koss@gmail.com>
2021-07-01 12:58:51 -0400

    move sendgrid logic into fn blocks

--- functions/index.js
+++ functions/index.js
@@ -8,33 +8,9 @@ const SENDGRID_API_KEY = functions.config().sendgrid.key;
 const SENDGRID_SENDER = functions.config().sendgrid.sender;
 const NEW_USER_EMAIL_TEMPLATE_ID = "d-7a1bc4cdc3d84efdab47c7e2edf6909d";
 const NEW_FLOWER_TEMPLATE_ID = "d-dca0af08c28148b5975bdc311c557121";
-const HAPPY_BIRTHDAY_EMAIL_TEMPLATE_ID = "d-7930e6c61d964ab19f63ea031e312701"
+const HAPPY_BIRTHDAY_EMAIL_TEMPLATE_ID = "d-7930e6c61d964ab19f63ea031e312701";
 
 sgMail.setApiKey(SENDGRID_API_KEY);
-
-const doSendEmail = async (to, templateId, dynamic_template_data) => {
-  try {
-    const msg = {
-      to,
-      from: SENDGRID_SENDER,
-      templateId,
-      dynamic_template_data,
-    };
-
-    functions.logger.info(
-      "Sending new flower email to " +
-        userData.email +
-        " " +
-        JSON.stringify(msg)
-    );
-
-    const [response] = await sgMail.send(msg);
-
-    functions.logger.info("Email sent.");
-  } catch (error) {
-    functions.logger.error(JSON.stringify(error));
-  }
-};
 
 exports.flowerOnCreate = functions.firestore
   .document("/user/{userId}/flower/{flowerId}")
@@ -46,10 +22,32 @@ exports.flowerOnCreate = functions.firestore
       .doc(ctx.params.userId)
       .get()
       .then((doc) => (userData = doc.data()));
-    await doSendEmail(userData.email, NEW_FLOWER_TEMPLATE_ID, {
-      subject: "Nice work! You created a flower!",
-      name: userData.name,
-    });
+
+    try {
+      const msg = {
+        to: userData.email,
+        from: SENDGRID_SENDER,
+        templateId: NEW_FLOWER_TEMPLATE_ID,
+        dynamic_template_data: {
+          subject: "Nice work! You created a flower!",
+          name: userData.name,
+        },
+      };
+
+      functions.logger.info(
+        "Sending new flower email to " +
+          userData.email +
+          " " +
+          JSON.stringify(msg)
+      );
+
+      const [response] = await sgMail.send(msg);
+
+      functions.logger.info("Email sent.");
+    } catch (error) {
+      functions.logger.error(JSON.stringify(error));
+    }
+
     return { success: true };
   });
 
@@ -57,10 +55,32 @@ exports.newUserEmail = functions.firestore
   .document("user/{userId}")
   .onCreate(async (snap, ctx) => {
     const userData = snap.data();
-    await doSendEmail(userData.email, NEW_USER_EMAIL_TEMPLATE_ID, {
-      subject: "Welcome to flowers.community",
-      name: userData.name,
-    });
+    functions.logger.info(JSON.stringify({ userData, params: ctx.params }));
+
+    try {
+      const msg = {
+        to: userData.email,
+        from: SENDGRID_SENDER,
+        templateId: NEW_USER_EMAIL_TEMPLATE_ID,
+        dynamic_template_data: {
+          subject: "Welcome to flowers.community",
+          name: userData.name,
+        },
+      };
+
+      functions.logger.info(
+        "Sending new flower email to " +
+          userData.email +
+          " " +
+          JSON.stringify(msg)
+      );
+
+      const [response] = await sgMail.send(msg);
+
+      functions.logger.info("Email sent.");
+    } catch (error) {
+      functions.logger.error(JSON.stringify(error));
+    }
     return { success: true };
   });
 
@@ -80,10 +100,31 @@ exports.happyBirthday = functions.pubsub
       .then((querySnapshot) => {
         querySnapshot.forEach(async (doc) => {
           const userData = doc.data();
-          doSendEmail(userData.email, HAPPY_BIRTHDAY_EMAIL_TEMPLATE_ID, {
-            subject: "Happy Birthday from flowers.community",
-            name: userData.name,
-          });
+
+          try {
+            const msg = {
+              to: userData.email,
+              from: SENDGRID_SENDER,
+              templateId: HAPPY_BIRTHDAY_EMAIL_TEMPLATE_ID,
+              dynamic_template_data: {
+                subject: "Happy Birthday from flowers.community",
+                name: userData.name,
+              },
+            };
+
+            functions.logger.info(
+              "Sending new flower email to " +
+                userData.email +
+                " " +
+                JSON.stringify(msg)
+            );
+
+            const [response] = await sgMail.send(msg);
+
+            functions.logger.info("Email sent.");
+          } catch (error) {
+            functions.logger.error(JSON.stringify(error));
+          }
         });
       })
       .catch((error) => {
