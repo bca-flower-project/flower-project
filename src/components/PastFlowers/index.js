b1fd8465570c3acbb1569ecd6cd3927a745aa4fc
iSWATxJOKERi <iswatxjokeri@gmail.com>
2024-01-30 18:23:52 -0800

    update view section

--- src/components/PastFlowers/index.js
+++ src/components/PastFlowers/index.js
@@ -6,13 +6,18 @@ import fire from "../../config/fire";
 import BlankFlower from "../BlankFlower";
 import QuestionsAnswers from "./QuestionsAnswers";
 import "./PastFlowers.scss";
+import { AppThemeContext } from "../../contexts/AppThemeContext";
 
 const { database } = fire;
 
 export default function PastFlowers(props) {
+  const { theme } = useContext(AppThemeContext);
   const [previousFlowers, setPreviousFlowers] = useState([]);
   const [loading, setLoading] = useState(false);
   const { currentUser } = useContext(AuthContext);
+  const [selectedGroup, setSelectedGroup] = useState("my-flowers");
+  const [displaySortFilter, setDisplaySortFilter] = useState(false);
+  const [sortFlowers, setSortFlowers] = useState("desc");
 
   useEffect(() => {
     window.scrollTo(0, 0);
@@ -44,8 +49,118 @@ export default function PastFlowers(props) {
     return <h1>Loading....</h1>;
   }
 
+  const toggleSortFilter = async (e) => {
+    e.preventDefault();
+    if(displaySortFilter) {
+      setDisplaySortFilter(false);
+    } else {
+      setDisplaySortFilter(true);
+    }
+  };
+
+  const toggleFlowers = async (group) => {
+    if(group == "my-flowers") {
+      setSelectedGroup("my-flowers");
+    } else if(group == "sent-flowers") {
+      setSelectedGroup("sent-flowers");
+    } else if(group == "received-flowers") {
+      setSelectedGroup("received-flowers");
+    } else {
+      setSelectedGroup("all-flowers");
+    }
+  }
+
+  const toggleSort = async (sort) => {
+    if((sortFlowers === "asc") && (sort === "desc")) {
+      setSortFlowers("desc");
+      setPreviousFlowers(previousFlowers.reverse());
+    } else if((sortFlowers === "desc") && (sort === "asc")){
+      setSortFlowers("asc");
+      setPreviousFlowers(previousFlowers.reverse());
+    }
+  }
+
   return (
-    <Container>
+    <Container fluid className={`View ${theme}`}
+      style={{
+        maxWidth: "1400px"
+      }}
+    >
+      <div className="show-parent-container">
+        <div
+          style={{
+            display: "flex",
+            alignItems: "center"
+          }}
+          className="show-parent-child-container"
+        >
+          <span className="show-label">Show:</span>
+          <div className="show-container">
+            <div className="show-btn"
+              style={{
+                border: selectedGroup == "my-flowers" ? "solid 1px yellow" : "solid 1px"
+              }}
+              onClick={(e) => {
+                e.preventDefault();
+                toggleFlowers("my-flowers");
+              }}
+            >My Flowers</div>
+            <div className="show-btn"
+              style={{
+                border: selectedGroup == "sent-flowers" ? "solid 1px yellow" : "solid 1px"
+              }}
+              onClick={(e) => {
+                e.preventDefault();
+                toggleFlowers("sent-flowers");
+              }}
+            >Sent Flowers</div>
+            <div className="show-btn"
+              style={{
+                border: selectedGroup == "received-flowers" ? "solid 1px yellow" : "solid 1px"
+              }}
+              onClick={(e) => {
+                e.preventDefault();
+                toggleFlowers("received-flowers");
+              }}
+            >Received Flowers</div>
+            <div className="show-btn"
+              style={{
+                border: selectedGroup == "all-flowers" ? "solid 1px yellow" : "solid 1px"
+              }}
+              onClick={(e) => {
+                e.preventDefault();
+                toggleFlowers("all-flowers");
+              }}
+            >All</div>
+          </div>
+        </div>
+        <div className="sort-container" onClick={toggleSortFilter}>
+          <div className="sort-btn">Sort by Date</div>
+          <div className="sort-btn">&#9660;</div>
+          {displaySortFilter ? <div className="sort-filters">
+            <div 
+              className="sort-filters-child"
+              onClick={(e) => {
+                e.preventDefault();
+                toggleSort("desc");
+              }}
+              style={{
+                border: sortFlowers == "desc" ? "solid 1px yellow" : "solid 1px"
+              }}
+            >Newest to Oldest</div>
+            <div 
+              className="sort-filters-child"
+              onClick={(e) => {
+                e.preventDefault();
+                toggleSort("asc");
+              }}
+              style={{
+                border: sortFlowers == "asc" ? "solid 1px yellow" : "solid 1px"
+              }}
+            >Oldest to Newest</div>
+          </div> : <></>}
+        </div>
+      </div>
       {previousFlowers.map((flower, index) => {
         const {
           id,
@@ -94,8 +209,8 @@ export default function PastFlowers(props) {
         date.setSeconds(flower.createdAt.seconds);
 
         return (
-          <Row key={`flower-${index}`} className="flowerRow">
-            <div className="col-sm-6">
+          <div key={`flower-${index}`} className="flower-row">
+            <div className="flower">
               <BlankFlower
                 colorOne={PeaksColor}
                 colorTwo={ChallengesColor}
@@ -105,7 +220,7 @@ export default function PastFlowers(props) {
                 colorSix={AspirationsColor}
               />
             </div>
-            <div className="col-sm-6">
+            <div className="flower-response">
               {!isValid && <Link to={`/create/${id}/edit`}>Edit</Link>}
               <QuestionsAnswers
                 date={date}
@@ -123,7 +238,7 @@ export default function PastFlowers(props) {
                 challengesAnswer={Challenges}
               />
             </div>
-          </Row>
+          </div>
         );
       })}
     </Container>
